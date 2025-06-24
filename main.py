577f0251ce337ed091f54df7ff200b3d17440063
Hamdan Albishi <hamdan.s.albishi@gmail.com>
2025-06-24 10:14:39 +0300

    fix uvicorn run

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=settings.debug
