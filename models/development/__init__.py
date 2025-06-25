d826b10b51d32ceb7b1e7034ee6a5bb182f72b55
Hamdan Albishi <117101410+HamdanXP@users.noreply.github.com>
2025-06-25 09:32:39 +0300

    Fix development package exports

--- models/development/__init__.py
+++ models/development/__init__.py
@@ -1,3 +1,6 @@
+"""Development models package."""
+
+from .models import *  # noqa: F401,F403 - re-export models for package users
 from .enums import AITechnique, DeploymentStrategy, ComplexityLevel
 
 __all__ = [
